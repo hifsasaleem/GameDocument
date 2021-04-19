cc65a44ba6e053c5072ea62b96cc5dd6561ea8fb
Ayesha <80509631+anumayesha@users.noreply.github.com>
2021-04-19 21:32:09 +0500

    Add files via upload

---
+++ scripts/GamePlayUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GamePlayUIManager : MonoBehaviour
+{
+
+    public GameObject GamePauseMode;
+    public GameObject GamePlayMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+              GamePauseMode.SetActive(false);
+        
+    }
+
+    public void OnClickPausebutton(){
+        GamePlayMode.SetActive(false);
+      GamePauseMode.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+
+    public void OnClickResumebutton(){
+      GamePauseMode.SetActive(false);
+      GamePlayMode.SetActive(true);
+
+        Time.timeScale = 1;
+    }
+
+}
---
+++ scripts/MainMenuUIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PLayBtnOnClick(){
+         SceneManager.LoadScene("GamePlayScene");
+
+    }
+}
